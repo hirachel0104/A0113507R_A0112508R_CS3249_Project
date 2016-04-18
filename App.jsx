99567b261c75acf32c7828089ba8142d8eb1f92d
Maple Hu Hsin Jo <hirachel0104@gmail.com>
2016-04-19 00:49:26 +0800

    Added create event with sign-in
    Can add items with sign in

--- App.jsx
+++ App.jsx
@@ -46,32 +46,73 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>CS3249 Project: Remake- IVLE Events</h1>
-            <AccountsUIWrapper />
+            <div> 
+                        <AccountsUIWrapper />
             { this.props.currentUser ?
              <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
-                    Enter Event Title:
+                        <h4>Event Title</h4>
             <input
               type="text"
               ref="eventTitle"
               placeholder="Event Title"
-            />
-                    Enter Organiser Name:
+            /><br></br>
+                        <h4>Organiser</h4>
             <input
               type="text"
               ref="organiser"
               placeholder="Organiser Name"
-            />
-                    Enter Date and Time:
+            /><br></br>
+                        <h4>Committee</h4>
+                        <select className="dropdown" name="comm">
+            <option value="c1">NUSSU</option>
+            <option value="c2">Faculty Clubs</option>
+            <option value="c3">Halls of Residencies</option>
+            <option value="c4">Clubs and Societies</option>
+            <option value="c5">NUS</option>
+            <option value="c6">Interest Groups</option>
+            <option value="c7">Others</option>
+            </select><br></br>
+                        <h4>Category</h4>
+            <select className="dropdown" name="comm">
+            <option value="1">Bashes</option>
+            <option value="2">FBazaars</option>
+            <option value="3">Competitions/Tournament</option>
+            <option value="4">Sports and Recreation</option>
+            <option value="5">Performances</option>
+            <option value="6">Annoucements</option>
+            <option value="7">Others</option>
+            </select><br></br>
+                        <h4>Tags</h4>
             <input
-              type="text"
-              ref="date"
-              placeholder="Date and Time"
-            />
+                type="text"
+                   /><br></br>
+                        
+                        <h4>Display Start</h4>
+            <input
+                type="text"
+                ref="date"
+                placeholder="date and time"
+                   />
+            <ul class="sidenote">(One week processing time.)</ul><br></br>
+                        <h4>Display End</h4>
+            <input
+                type="text"
+                   />
+             <ul class="sidenote">(Max 14days or 1 day after event.)</ul><br></br>
+               <h4>Description</h4>
+            <input
+                type="text"
+                placeholder="ckeditor-to-be-installed"
+                   />
+            <br></br>
             <input type="submit" value="Submit"></input>
           </form>:''
             }
+            </div>
+            
             </header>
-                           
+               
+            
     <div id="navBar">
         <button id="createEvent">+ Create Event</button>
         <form>
