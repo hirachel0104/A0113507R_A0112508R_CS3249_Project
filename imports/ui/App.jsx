8278886179352087b8a1a3277682ab54d9529b73
Hu Hsin Jo <hirachel0104@gmail.com>
2016-04-20 01:37:58 +0800

    App.jsx updated with form

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -41,6 +41,9 @@ class App extends Component {
       <Task key={task._id} task={task} />
     ));
   }
+    
+    createSth(){
+    }
  
   render() {
     return (
@@ -55,14 +58,105 @@ class App extends Component {
             { this.props.currentUser ?
                     <div>
                         
+                    <div>
+        <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+                        <h2 className="createInner">General</h2>
+                        <h4 className="createInner">Event Title</h4>
+            <input
+              type="text"
+              ref="eventTitle"
+              placeholder="Event Title"
+            /><br></br>
+                        <h4 className="createInner">Organiser</h4>
+            <input
+              type="text"
+              ref="organiser"
+              placeholder="Organiser Name"
+            /><br></br>
+                        <h4 className="createInner">Committee</h4>
+                        <select className="dropdown" name="comm">
+            <option value="c1">NUSSU</option>
+            <option value="c2">Faculty Clubs</option>
+            <option value="c3">Halls of Residencies</option>
+            <option value="c4">Clubs and Societies</option>
+            <option value="c5">NUS</option>
+            <option value="c6">Interest Groups</option>
+            <option value="c7">Others</option>
+            </select><br></br>
+                        <h4 className="createInner">Category</h4>
+            <select className="dropdown" name="comm">
+            <option value="1">Bashes</option>
+            <option value="2">FBazaars</option>
+            <option value="3">Competitions/Tournament</option>
+            <option value="4">Sports and Recreation</option>
+            <option value="5">Performances</option>
+            <option value="6">Annoucements</option>
+            <option value="7">Others</option>
+            </select><br></br>
+                        <h4 className="createInner">Tags</h4>
+            <input 
+                type="text"
+                   /><br></br>
+                        
+                        <h4 className="createInner">Display Start</h4>
+            <input
+                type="text"
+                   />
+            <ul>(One week processing time.)</ul><br></br>
+                        <h4 className="createInner">Display End</h4>
+            <input
+                type="text"
+                   />
+             <ul>(Max 14days or 1 day after event.)</ul><br></br>
+               <h4>Description</h4>
+            <input
+                type="text"
+                placeholder="ckeditor-to-be-installed"
+                   />
+            <br></br>
+            
+            <h2 className="createInner">Event Details</h2>
+                        <h4 className="createInner">Date snd Time</h4>
+                      <input
+                type="text"
+                ref="date"
+                placeholder="date and time"
+                   />
+            <br></br>  
+                        <h4 className="createInner">Venue</h4>
+                      <input
+                type="text"
+                   />
+            <br></br>     
+                        <h4 className="createInner">Price</h4>
+                      <input
+                type="text"
+                   />
+            <br></br>  
+                        <h4 className="createInner">Agenda</h4>
+                      <input
+                type="text"
+                   />
+            <br></br>  
+                        <h4 className="createInner">Contact</h4>
+                      <input
+                type="text"
+                   />
+            <br></br>  
+                        
+            <input id="createEvent" type="submit" value="+ Create Event"></input>
+          </form>
+        </div>
+                        
                    <div id="navBar">
-        <button id="createEvent">+ Create Event</button>
         <form>
         <input id="search"
             type="text"
             ref="searchInput"
             placeholder="Search"
             />
+        <input id="searchIcon" type="submit" value="Search"></input>
+        <input onClick={this.props.onClick} id="searchIcon" type="button" value="Uodo"></input>
         </form>
         <p id="totalDisplay">Total {this.props.totalCount} items</p>
             </div>
@@ -80,13 +174,12 @@ class App extends Component {
         <strong><h4>Event Date</h4></strong>
     </div>
             
-        <ul>
           {this.renderTasks()}
-        </ul>
             </div>
-                        
+     
             </div>:''
             }
+                {this.createSth()}
                 
             </div>
       </div>
